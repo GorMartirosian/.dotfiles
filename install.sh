b3dbec7cbf354c654a5a7615ec15b3f9942ea50a
GorMartirosian <gormartiros2002@gmail.com>
2025-08-20 23:55:32 +0400

    Now install.sh overrides the files/directories to be symlinks to .dotfiles

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+# Ensure ~/.local/bin exists
+if [ ! -d "$HOME/.local/bin" ]; then
+    mkdir "$HOME/.local/bin"
+fi
+
 # Symlink files
-ln -sf ~/.dotfiles/.bashrc ~/.bashrc
-ln -sf ~/.dotfiles/.vimrc ~/.vimrc
-ln -sf ~/.dotfiles/.local/bin/ql-https ~/.local/bin/ql-https
+ln -sfn ~/.dotfiles/.bashrc ~/.bashrc
+ln -sfn ~/.dotfiles/.vimrc ~/.vimrc
+ln -sfn ~/.dotfiles/.local/bin/ql-https ~/.local/bin/ql-https
 
 # Symlink directories
 ln -sfn ~/.dotfiles/.emacs.d ~/.emacs.d
